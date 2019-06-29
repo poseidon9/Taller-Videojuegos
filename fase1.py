16732a8a7c32ef81343e6f956d0fd8efa5353f3a
poseidon9 <poseidonthenine@gmail.comm>
2019-06-29 08:14:40 +0800

    fase1

---
+++ fase1.py
@@ -0,0 +1,25 @@
+import sys, random
+
+print("Fase1")
+
+score = 0
+
+while(1):
+	print("1:Jugar\n2:Salir")
+	opc = raw_input()
+
+	if opc == "1":
+		throw = random.randint(1,6)
+		print("Adivina el dado 1-6\n")
+		guess = int(raw_input())
+		if throw == guess:
+			score += 1
+			print("ACERTASTE!, punctuacion actual: "+str(score))
+		print("el dado tirado fue: "+str(throw))
+	elif opc == "2":
+		print("Se acabo, Su punctuacion Final fue: "+str(score))
+		break
+	else:
+		print("No existe Dicha Opcion")
+
+
